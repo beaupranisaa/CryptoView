75a7dffb86386c06fdd01d1cb7c33be1393db2f0
root <rom.parnichkun@gmail.com>
2021-04-20 11:14:13 +0000

    moved signals function to analytics folder

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -42,3 +42,69 @@ def macd_signals(df):
 def commodity_channel(df):
     cci_signal = df['trend_cci']
     return cci_signal
+
+def signal_indicator(close, values, macd_signal, ao_prev):
+    if values[0] > 100 and values[0] < 150:
+        signal_cci = 'SELL'
+    elif values[0] < -100 and values[0] > -150:
+        signal_cci = 'BUY'
+    elif values[0] >= 150:
+        signal_cci = 'STRONG SELL'
+    elif values[0] <= -150:
+        signal_cci = 'STRONG BUY'
+    else: 
+        signal_cci = 'NEUTRAL'
+
+    if values[1] > 0 and values[1] < 20:
+        signal_rsi = 'BUY'
+    elif values[1] > 80 and values[1] < 100:
+        signal_rsi = 'SELL'
+    elif values[1] > 100:
+        signal_rsi = 'STRONG SELL'
+    elif values[1] < 0:
+        signal_rsi = 'STRONG BUY'
+    else:
+        signal_rsi = 'NEUTRAL'
+
+    if close > values[2]:
+        signal_kama = 'BUY'
+    else: 
+        signal_kama = 'SELL'
+
+    if values[3] or values[4] > close:
+        signal_sma = 'BUY'
+        signal_ema = 'BUY'
+    elif values[3] == close and values[4] == close:
+        signal_sma = 'NEUTRAL'
+        signal_ema = 'NEUTRAL'
+    else:
+        signal_sma = 'SELL'
+        signal_ema = 'SELL'
+    
+    if values[5] > macd_signal:
+        signal_macd = 'BUY'
+    elif values[5] < macd_signal:
+        signal_macd = 'SELL'
+    else:
+        signal_macd = 'NEUTRAL'
+
+    if ao_prev < values[6]: 
+        signal_awesome = 'BUY'
+    elif ao_prev > values[6]: 
+        signal_awesome = 'SELL'
+    else:
+        signal_awesome = 'NEUTRAL'
+
+    if values[7] >= 0 and values[7] <= 30:
+        signal_ultimate = 'BUY'
+    elif values[7] < 0:
+        signal_ultimate = 'STRONG BUY'
+    elif values[7] >= 70 and values[7] < 100:
+        signal_ultimate = 'SELL'
+    elif values[7] >= 100:
+        signal_ultimate = 'STRONG SELL'
+    else:
+        signal_ultimate = 'NEUTRAL'
+    signals = [signal_cci, signal_rsi, signal_kama, signal_sma, 
+                        signal_ema, signal_macd, signal_awesome, signal_ultimate] 
+    return signals
--- ui/custom_dash_components.py
+++ ui/custom_dash_components.py
@@ -410,74 +410,6 @@ technicals = html.Div(children = [bullet_graph],
             style={'width': '100%', 'display': 'flex',"paddingLeft" : 70, "paddingTop" : 50,
             "paddingBottom": 60})
 
-def signal_indicator(close, values, macd_signal, ao_prev):
-    if values[0] > 100 and values[0] < 150:
-        signal_cci = 'SELL'
-    elif values[0] < -100 and values[0] > -150:
-        signal_cci = 'BUY'
-    elif values[0] >= 150:
-        signal_cci = 'STRONG SELL'
-    elif values[0] <= -150:
-        signal_cci = 'STRONG BUY'
-    else: 
-        signal_cci = 'NEUTRAL'
-
-    if values[1] > 0 and values[1] < 20:
-        signal_rsi = 'BUY'
-    elif values[1] > 80 and values[1] < 100:
-        signal_rsi = 'SELL'
-    elif values[1] > 100:
-        signal_rsi = 'STRONG SELL'
-    elif values[1] < 0:
-        signal_rsi = 'STRONG BUY'
-    else:
-        signal_rsi = 'NEUTRAL'
-
-    if close > values[2]:
-        signal_kama = 'BUY'
-    else: 
-        signal_kama = 'SELL'
-
-    if values[3] or values[4] > close:
-        signal_sma = 'BUY'
-        signal_ema = 'BUY'
-    elif values[3] == close and values[4] == close:
-        signal_sma = 'NEUTRAL'
-        signal_ema = 'NEUTRAL'
-    else:
-        signal_sma = 'SELL'
-        signal_ema = 'SELL'
-    
-    if values[5] > macd_signal:
-        signal_macd = 'BUY'
-    elif values[5] < macd_signal:
-        signal_macd = 'SELL'
-    else:
-        signal_macd = 'NEUTRAL'
-
-    if ao_prev < values[6]: 
-        signal_awesome = 'BUY'
-    elif ao_prev > values[6]: 
-        signal_awesome = 'SELL'
-    else:
-        signal_awesome = 'NEUTRAL'
-
-    if values[7] >= 0 and values[7] <= 30:
-        signal_ultimate = 'BUY'
-    elif values[7] < 0:
-        signal_ultimate = 'STRONG BUY'
-    elif values[7] >= 70 and values[7] < 100:
-        signal_ultimate = 'SELL'
-    elif values[7] >= 100:
-        signal_ultimate = 'STRONG SELL'
-    else:
-        signal_ultimate = 'NEUTRAL'
-    signals = [signal_cci, signal_rsi, signal_kama, signal_sma, 
-                        signal_ema, signal_macd, signal_awesome, signal_ultimate] 
-    return signals
-
-
-
 def create_bullet_graph(data, weights):
     close = data['close'][0]
     macd_signal = data['trend_macd_signal'][0]
@@ -485,7 +417,7 @@ def create_bullet_graph(data, weights):
     data = np.round(data[['trend_cci', 'momentum_rsi', 'momentum_kama', 'trend_sma_fast', 'trend_ema_fast', 'trend_macd', 'momentum_ao', 'momentum_uo']], 2)
     data = np.transpose(data).iloc[:, 0]
     values = np.array(data)
-    signals = signal_indicator(close, values, macd_signal, ao_prev)
+    signals = analytics.signal_indicator(close, values, macd_signal, ao_prev)
 #    total_buy = ((1.5 * signals.count('SELL')) + (2 * signals.count('STRONG SELL')) + 
 #                    signals.count('NEUTRAL'))/(len(signals)) #Need to change this, for initialization only
     
@@ -632,7 +564,7 @@ def indicators_table(data):
     data = np.round(data[['trend_cci', 'momentum_rsi', 'momentum_kama', 'trend_sma_fast', 'trend_ema_fast', 'trend_macd', 'momentum_ao', 'momentum_uo']], 2)
     data = np.transpose(data).iloc[:, 0]
     values = np.array(data)
-    signals = signal_indicator(close, values, macd_signal, ao_prev)
+    signals = analytics.signal_indicator(close, values, macd_signal, ao_prev)
     indicators = ['Trend CCI', 'Relative Strength', 'Kaufmans Average', 'Simple MA', 'Exponential MA', 'MACD', 'Awesome Oscillator', 'Ultimate Oscillator']
     columns = ['Indicators', '24H Values', '24H Signals',  'Simple MA', 'Exponential MA', 'MACD', 'Awesome Oscillator', 'Ultimate Oscillator']
     df = pd.DataFrame(data = [indicators, values, signals], columns = columns)
